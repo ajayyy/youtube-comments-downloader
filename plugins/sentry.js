b5fade96905f33398dbfd29f909bf5be08bd4d3d
Bartek Igielski <igloczek@gmail.com>
2020-03-14 17:45:22 +0100

    Sentry update

--- plugins/sentry.js
+++ plugins/sentry.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
-import Raven from 'raven-js'
-import RavenVue from 'raven-js/plugins/vue'
+import * as Sentry from '@sentry/browser';
+import * as Integrations from '@sentry/integrations';
 
-Raven
-  .config('https://b65ddccb9c594cbcb1d6a114d7d9d5c4@sentry.io/152451')
-  .addPlugin(RavenVue, Vue)
-  .install()
+Sentry.init({
+  dsn: 'https://b65ddccb9c594cbcb1d6a114d7d9d5c4@sentry.io/152451',
+  integrations: [
+    new Integrations.Vue({ Vue, attachProps: true })
+  ],
+  release: 'youtube-comments-downloader@' + process.env.npm_package_version
+});
